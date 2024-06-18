9f3f5e642abccee5db07aa001957e599c100bb81
Anubhav <lucimgstar007@gmail.com>
2024-06-18 12:26:23 +0530

    Custom not found page and notfound function covered

---
+++ routing-demo/src/app/not-found.tsx
@@ -0,0 +1,23 @@
+const NotFound: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-indigo-500 to-purple-600">
+      <div className="max-w-md text-center p-8 bg-white rounded-xl shadow-md">
+        <h1 className="text-4xl font-bold text-gray-800">Oops! We got lost.</h1>
+        <p className="text-gray-600 mt-4">
+          The page you're looking for could not be found. It might be
+          unavailable temporarily, or it might have moved.
+        </p>
+        <div className="mt-8 flex justify-center">
+          <a
+            href="/"
+            className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Go Home
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,12 @@
+import { notFound } from "next/navigation";
 const ReviewDetail = ({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) => {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <div>
       <h1>
